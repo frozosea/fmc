d939e351db23bc4a5b96735dd4c55f78aac66546
dizolo <y20072004@gmail.com>
2022-07-08 21:15:09 +1000

    fix

--- container-tracking/src/trackTrace/trackingBybillNumber/sitc/sitc.ts
+++ container-tracking/src/trackTrace/trackingBybillNumber/sitc/sitc.ts
@@ -7,13 +7,16 @@ import {fetchArgs} from "../../helpers/requestSender";
 import SitcBillNumberApiResponseSchema from "./sitcApiResponseSchema";
 import {IDatetime} from "../../helpers/datetime";
 import {NotThisShippingLineException} from "../../../exceptions";
+import {SitcContainerTrackingApiResponseSchema} from "../../TrackingByContainerNumber/sitc/sitcApiResponseSchema";
 
 export interface ISitcBillNumberRequest {
-    getApiResponse(args: { billNo: string, solvedCaptcha: string, randomString: string }): Promise<SitcBillNumberApiResponseSchema>
+    getBillNoResponse(args: { billNo: string, solvedCaptcha: string, randomString: string }): Promise<SitcBillNumberApiResponseSchema>;
+
+    getContainerInfo(args: { billNo: string, containerNo: string }): Promise<SitcContainerTrackingApiResponseSchema>;
 }
 
-export class SitcBillNumberRequest extends SitcRequest {
-    public async getApiResponse(args: { billNo: string, solvedCaptcha: string, randomString: string }): Promise<SitcBillNumberApiResponseSchema> {
+export class SitcBillNumberRequest extends SitcRequest implements ISitcBillNumberRequest {
+    public async getBillNoResponse(args: { billNo: string, solvedCaptcha: string, randomString: string }): Promise<SitcBillNumberApiResponseSchema> {
         return await this.request.sendRequestAndGetJson({
             url: `http://api.sitcline.com/doc/cargoTrack/searchApp?blNo=${args.billNo}&code=${args.solvedCaptcha}&randomStr=${args.randomString}`,
             method: "POST",
@@ -23,7 +26,23 @@ export class SitcBillNumberRequest extends SitcRequest {
                 // "cookie": "rememberMe=true; username=flyasea; Hm_lvt_6c6c344c19846b08289aece9f968bd6f=1655363989,1657166543; password=qCbbbVcqDxGb6GMzs6bWLk11MXsF5K0xgTumeXo1GX/4BbpCdWZ8y0ZMRBXntKfj3pr/vwghgsRtcYqLKiLFUA==; Hm_lpvt_6c6c344c19846b08289aece9f968bd6f=1657266306",
                 "Referer": "http://api.sitcline.com/app/cargoTrackSearch",
                 "Referrer-Policy": "strict-origin-when-cross-origin"
-            }
+            },
+            body: null
+        })
+    }
+
+    public async getContainerInfo(args: { billNo: string, containerNo: string }): Promise<SitcContainerTrackingApiResponseSchema> {
+        return await this.request.sendRequestAndGetJson({
+            url: `http://api.sitcline.com/doc/cargoTrack/movementDetailApp?blNo=${args.billNo}&containerNo=${args.containerNo}`,
+            method: "POST",
+            headers: {
+                "accept": "application/json, text/plain, */*",
+                "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh-CN;q=0.5,zh;q=0.4",
+                // "cookie": "rememberMe=true; username=flyasea; Hm_lvt_6c6c344c19846b08289aece9f968bd6f=1655363989,1657166543; password=qCbbbVcqDxGb6GMzs6bWLk11MXsF5K0xgTumeXo1GX/4BbpCdWZ8y0ZMRBXntKfj3pr/vwghgsRtcYqLKiLFUA==; Hm_lpvt_6c6c344c19846b08289aece9f968bd6f=1657266306",
+                "Referer": "http://api.sitcline.com/app/cargoTrack",
+                "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            body: null
         })
     }
 }
@@ -76,14 +95,17 @@ export class SitcBillNumber extends SitcContainer implements IBillNumberTracker 
         } catch (e) {
             throw new NotThisShippingLineException();
         }
-        let response: SitcBillNumberApiResponseSchema = await this.billRequest.getApiResponse({
+        let response: SitcBillNumberApiResponseSchema = await this.billRequest.getBillNoResponse({
             billNo: args.number,
             solvedCaptcha: solvedCaptcha,
             randomString: randomString
         })
         let eta: number = this.etaParser.getEta(response)
         let containerNumber: string = this.containerNumberParser.getNumber(response)
-        let containerNumberTrackingResponse = await this.request.getApiResponseJson({number: containerNumber})
+        let containerNumberTrackingResponse = await this.billRequest.getContainerInfo({
+            billNo: args.number,
+            containerNo: containerNumber
+        })
         let infoAboutMoving: OneTrackingEvent[] = this.infoAboutMovingParser.getInfoAboutMoving(containerNumberTrackingResponse)
         return {billNo: args.number, scac: "SITC", infoAboutMoving: infoAboutMoving, etaFinalDelivery: eta}
 
