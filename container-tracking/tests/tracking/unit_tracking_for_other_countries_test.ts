ee9c3faf5ab1fe4a858e339453485d7bc3e34e95
dizolo <y20072004@gmail.com>
2022-07-24 18:15:18 +1000

    fix tests

--- container-tracking/tests/tracking/unit_tracking_for_other_countries_test.ts
+++ container-tracking/tests/tracking/unit_tracking_for_other_countries_test.ts
@@ -46,13 +46,13 @@ export class FesoMoch extends BaseTrackerByContainerNumber<fetchArgs> {
                 containerSize: '20DC',
                 infoAboutMoving: [
                     {
-                        time: 1654495200000,
+                        time: new Date().getTime(),
                         operationName: 'Gate out empty for loading',
                         location: 'MAGISTRAL',
                         vessel: ''
                     },
                     {
-                        time: 1654588800000,
+                        time: new Date().getTime(),
                         operationName: 'Gate in empty from consignee',
                         location: 'ZAPSIBCONT',
                         vessel: ''
@@ -74,44 +74,44 @@ export class SkluMoch extends FesoMoch {
                 scac: 'SKLU',
                 infoAboutMoving: [
                     {
-                        time: 1653906120000,
+                        time: new Date().getTime(),
                         operationName: 'Pickup (1/1)',
                         location: 'SINOKOR TAM CANG CAT LAI Depot',
                         vessel: ''
                     },
                     {
-                        time: 1653961680000,
+                        time: new Date().getTime(),
                         operationName: 'Return (1/1)',
                         location: 'CAT LAI',
                         vessel: ''
                     },
                     {
-                        time: 1654567200000,
+                        time: new Date().getTime(),
                         operationName: 'Departure',
                         location: 'CAT LAI',
                         vessel: 'HEUNG-A HOCHIMINH / 2205N'
                     },
                     {
-                        time: 1655085600000,
+                        time: new Date().getTime(),
                         operationName: 'Arrival(T/S) (Scheduled)',
                         location: 'BPTS',
                         vessel: 'HEUNG-A HOCHIMINH / 2205N'
                     },
                     {
-                        time: 1655283600000,
+                        time: new Date().getTime(),
                         operationName: 'Departure(T/S) (Scheduled)',
                         location: 'BPTS',
                         vessel: 'HEUNG-A ULSAN / 2256E'
                     },
                     {
-                        time: 1655452800000,
+                        time: new Date().getTime(),
                         operationName: 'Arrival (Scheduled)',
                         location: 'HOSOSHIMA TERMINAL(SHIRAHMA #14)',
                         vessel: 'HEUNG-A ULSAN / 2256E'
                     },
                     {
                         operationName: 'ETA',
-                        time: 1655424000000,
+                        time: new Date().getTime(),
                         location: 'Hososhima',
                         vessel: ''
                     }
@@ -133,19 +133,19 @@ export class SitcMoch extends SkluMoch {
                 scac: 'SITC',
                 infoAboutMoving: [
                     {
-                        time: 1654380000000,
+                        time: new Date().getTime(),
                         operationName: 'LOADED ONTO VESSEL',
                         vessel: 'SITC CAGAYAN',
                         location: 'DALIAN'
                     },
                     {
-                        time: 1653649140000,
+                        time: new Date().getTime(),
                         operationName: 'OUTBOUND PICKUP',
                         vessel: 'SITC CAGAYAN',
                         location: 'DALIAN'
                     },
                     {
-                        time: 1652899980000,
+                        time: new Date().getTime(),
                         operationName: 'EMPTY CONTAINER',
                         vessel: 'SITC MAKASSAR',
                         location: 'DALIAN'
@@ -166,79 +166,79 @@ export class OneyMoch extends FesoMoch {
                 scac: 'ONEY',
                 infoAboutMoving: [
                     {
-                        time: 1648120740000,
+                        time: new Date().getTime(),
                         operationName: 'Empty Container Release to Shipper',
                         location: 'PUSAN, KOREA REPUBLIC OF',
                         vessel: ''
                     },
                     {
-                        time: 1649155200000,
+                        time: new Date().getTime(),
                         operationName: 'Gate In to Outbound Terminal',
                         location: 'PUSAN, KOREA REPUBLIC OF',
                         vessel: ''
                     },
                     {
-                        time: 1649333340000,
+                        time: new Date().getTime(),
                         operationName: "Loaded on 'HYUNDAI SINGAPORE 126E' at Port of Loading",
                         location: 'PUSAN, KOREA REPUBLIC OF',
                         vessel: 'HYUNDAI SINGAPORE'
                     },
                     {
-                        time: 1649365200000,
+                        time: new Date().getTime(),
                         operationName: "'HYUNDAI SINGAPORE 126E' Departure from Port of Loading",
                         location: 'PUSAN, KOREA REPUBLIC OF',
                         vessel: 'HYUNDAI SINGAPORE'
                     },
                     {
-                        time: 1650297600000,
+                        time: new Date().getTime(),
                         operationName: "'HYUNDAI SINGAPORE 126E' Arrival at Port of Discharging",
                         location: 'VANCOUVER, BC, CANADA',
                         vessel: 'HYUNDAI SINGAPORE'
                     },
                     {
-                        time: 1653707640000,
+                        time: new Date().getTime(),
                         operationName: "'HYUNDAI SINGAPORE 126E' POD Berthing Destination",
                         location: 'VANCOUVER, BC, CANADA',
                         vessel: 'HYUNDAI SINGAPORE'
                     },
                     {
-                        time: 1654033620000,
+                        time: new Date().getTime(),
                         operationName: "Unloaded from 'HYUNDAI SINGAPORE 126E' at Port of Discharging",
                         location: 'VANCOUVER, BC, CANADA',
                         vessel: 'HYUNDAI SINGAPORE'
                     },
                     {
-                        time: 1654215840000,
+                        time: new Date().getTime(),
                         operationName: 'Loaded on rail at inbound rail origin',
                         location: 'VANCOUVER, BC, CANADA',
                         vessel: ''
                     },
                     {
-                        time: 1654240140000,
+                        time: new Date().getTime(),
                         operationName: 'Inbound Rail Departure',
                         location: 'VANCOUVER, BC, CANADA',
                         vessel: ''
                     },
                     {
-                        time: 1654799640000,
+                        time: new Date().getTime(),
                         operationName: 'Inbound Rail Arrival',
                         location: 'DETROIT, MI, UNITED STATES',
                         vessel: ''
                     },
                     {
-                        time: 1654808520000,
+                        time: new Date().getTime(),
                         operationName: 'Unloaded from rail at inbound rail destination',
                         location: 'DETROIT, MI, UNITED STATES',
                         vessel: ''
                     },
                     {
-                        time: 1654857480000,
+                        time: new Date().getTime(),
                         operationName: 'Gate Out from Inbound CY for Delivery to Consignee',
                         location: 'DETROIT, MI, UNITED STATES',
                         vessel: ''
                     },
                     {
-                        time: 1654863840000,
+                        time: new Date().getTime(),
                         operationName: 'Empty Container Returned from Customer',
                         location: 'DETROIT, MI, UNITED STATES',
                         vessel: ''
@@ -259,49 +259,49 @@ export class MaeuMoch extends OneyMoch {
                 scac: 'MAEU',
                 infoAboutMoving: [
                     {
-                        time: 1648536120000,
+                        time: new Date().getTime(),
                         location: 'Win Win Container Depot',
                         operationName: 'GATE-OUT-EMPTY',
                         vessel: 'MSC SVEVA'
                     },
                     {
-                        time: 1648598520000,
+                        time: new Date().getTime(),
                         location: 'Laem Chabang Terminal PORT D1',
                         operationName: 'GATE-IN',
                         vessel: 'MSC SVEVA'
                     },
                     {
-                        time: 1649859300000,
+                        time: new Date().getTime(),
                         location: 'Laem Chabang Terminal PORT D1',
                         operationName: 'LOAD',
                         vessel: 'MSC SVEVA'
                     },
                     {
-                        time: 1650854940000,
+                        time: new Date().getTime(),
                         location: 'YANGSHAN SGH GUANDONG TERMINAL',
                         operationName: 'DISCHARG',
                         vessel: 'MSC SVEVA'
                     },
                     {
-                        time: 1651417860000,
+                        time: new Date().getTime(),
                         location: 'YANGSHAN SGH GUANDONG TERMINAL',
                         operationName: 'LOAD',
                         vessel: 'ZIM WILMINGTON'
                     },
                     {
-                        time: 1654919640000,
+                        time: new Date().getTime(),
                         location: 'Charleston Wando Welch terminal N59',
                         operationName: 'DISCHARG',
                         vessel: 'ZIM WILMINGTON'
                     },
                     {
-                        time: 1655164800000,
+                        time: new Date().getTime(),
                         location: 'Charleston Wando Welch terminal N59',
                         operationName: 'GATE-OUT',
                         vessel: ''
                     },
                     {
-                        time: 1654919640000,
+                        time: new Date().getTime(),
                         operationName: 'ETA',
                         location: 'Spartanburg',
                         vessel: ''
@@ -322,19 +322,19 @@ export class MscuMoch extends MaeuMoch {
                 scac: 'MSCU',
                 infoAboutMoving: [
                     {
-                        time: 1654819200000,
+                        time: new Date().getTime(),
                         operationName: 'Export at barge yard',
                         location: 'CHONGQING, CN',
                         vessel: ''
                     },
                     {
-                        time: 1654732800000,
+                        time: new Date().getTime(),
                         operationName: 'Empty to Shipper',
                         location: 'CHONGQING, CN',
                         vessel: ''
                     },
                     {
-                        time: 1654992000000,
+                        time: new Date().getTime(),
                         operationName: 'ETA',
                         location: '',
                         vessel: ''
@@ -352,43 +352,43 @@ export class CosuMoch extends MscuMoch {
             return {
                 container: args.number, scac: "COSU", containerSize: "", infoAboutMoving: [
                     {
-                        time: 1654083120000,
+                        time: new Date().getTime(),
                         operationName: 'Empty Equipment Returned',
                         location: 'United Waalhaven Terminals BV(Gate2,Rotterdam,Zuid-Holland,Netherlands',
                         vessel: 'Truck'
                     },
                     {
-                        time: 1653893580000,
+                        time: new Date().getTime(),
                         operationName: 'Gate-out from Final Hub',
                         location: 'Euromax Terminal,Rotterdam,Zuid-Holland,Netherlands',
                         vessel: 'Truck'
                     },
                     {
-                        time: 1653253980000,
+                        time: new Date().getTime(),
                         operationName: 'Discharged at Last POD',
                         location: 'Euromax Terminal,Rotterdam,Zuid-Holland,Netherlands',
                         vessel: 'Vessel'
                     },
                     {
-                        time: 1649940720000,
+                        time: new Date().getTime(),
                         operationName: 'Loaded at First POL',
                         location: 'QQCTU Qingdao Qianwan United Ctn,Qingdao,Shandong,China',
                         vessel: 'Vessel'
                     },
                     {
-                        time: 1649671440000,
+                        time: new Date().getTime(),
                         operationName: 'Cargo Received',
                         location: 'QQCTU Qingdao Qianwan United Ctn,Qingdao,Shandong,China',
                         vessel: 'Truck'
                     },
                     {
-                        time: 1649671440000,
+                        time: new Date().getTime(),
                         operationName: 'Gate-In at First POL',
                         location: 'QQCTU Qingdao Qianwan United Ctn,Qingdao,Shandong,China',
                         vessel: 'Truck'
                     },
                     {
-                        time: 1649318100000,
+                        time: new Date().getTime(),
                         operationName: 'Empty Equipment Despatched',
                         location: "Qingdao Shenzhouxing Int'l Frt Co,Qingdao,Shandong,China",
                         vessel: 'Truck'
@@ -406,19 +406,19 @@ export class KmtuMoch extends CosuMoch {
             return {
                 container: args.number, scac: "KMTU", containerSize: "", infoAboutMoving: [
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container was picked',
                         location: 'BUSAN,Hutchison Busan Container Terminal',
                         vessel: ''
                     },
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container was arrived',
                         location: 'BUSAN,Busan Port Terminal',
                         vessel: ''
                     },
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container is onboard and is scheduled to arrive at transshipment',
                         location: 'BUSAN,Busan Port Terminal',
                         vessel: ''
@@ -436,19 +436,19 @@ export class HaluMoch extends CosuMoch {
             return {
                 container: args.number, scac: "HALU", containerSize: "", infoAboutMoving: [
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container was picked',
                         location: 'BUSAN,Hutchison Busan Container Terminal',
                         vessel: ''
                     },
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container was arrived',
                         location: 'BUSAN,Busan Port Terminal',
                         vessel: ''
                     },
                     {
-                        time: 1653436800000,
+                        time: new Date().getTime(),
                         operationName: 'container is onboard and is scheduled to arrive at transshipment',
                         location: 'BUSAN,Busan Port Terminal',
                         vessel: ''
