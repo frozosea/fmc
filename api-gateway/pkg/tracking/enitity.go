056cf80c731dca8b6b4eec308ab33dd066bfc9a6
dizolo <y20072004@gmail.com>
2022-08-06 19:15:37 +1000

    fix json response

--- api-gateway/pkg/tracking/enitity.go
+++ api-gateway/pkg/tracking/enitity.go
@@ -13,13 +13,13 @@ type BaseInfoAboutMoving struct {
 }
 type BillNumberResponse struct {
 	BillNo           string                `protobuf:"bytes,1,opt,name=billNo,proto3" json:"billNo,omitempty"`
-	Scac             string                `protobuf:"varint,3,opt,name=Scac,proto3,enum=tracking.Scac" json:"Scac,omitempty"`
+	Scac             string                `protobuf:"varint,3,opt,name=Scac,proto3,enum=tracking.Scac" json:"scac,omitempty"`
 	InfoAboutMoving  []BaseInfoAboutMoving `protobuf:"bytes,4,rep,name=info_about_moving,json=infoAboutMoving,proto3" json:"info_about_moving,omitempty"`
 	EtaFinalDelivery int64                 `protobuf:"varint,5,opt,name=eta_final_delivery,json=etaFinalDelivery,proto3" json:"eta_final_delivery,omitempty"`
 }
 type ContainerNumberResponse struct {
 	Container       string                `protobuf:"bytes,1,opt,name=container,proto3" json:"container,omitempty"`
 	ContainerSize   string                `protobuf:"bytes,2,opt,name=container_size,json=containerSize,proto3" json:"container_size,omitempty"`
-	Scac            string                `protobuf:"varint,3,opt,name=Scac,proto3,enum=tracking.Scac" json:"Scac,omitempty"`
+	Scac            string                `protobuf:"varint,3,opt,name=Scac,proto3,enum=tracking.Scac" json:"scac,omitempty"`
 	InfoAboutMoving []BaseInfoAboutMoving `protobuf:"bytes,4,rep,name=info_about_moving,json=infoAboutMoving,proto3" json:"info_about_moving,omitempty"`
 }
