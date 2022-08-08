8f0f43668c93d772176a56e2ef315692684ef773
dizolo <y20072004@gmail.com>
2022-08-08 14:57:30 +1000

    fix migrations

--- deployment/swarm/services/schedule-tracking/schema/000001_tables_create.up.sql
+++ deployment/swarm/services/schedule-tracking/schema/000001_tables_create.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS "tasks"
 (
-    id           SERIAL      NOT NULL,
-    number       VARCHAR(40) NOT NULL,
-    user_id      INT         NOT NULL,
-    country      VARCHAR(10) NOT NULL,
-    time         VARCHAR(5)  NOT NULL,
-    emails       VARCHAR[]   NOT NULL,
-    is_container BOOLEAN     NOT NULL
+    id            SERIAL       NOT NULL,
+    number        VARCHAR(40)  NOT NULL,
+    user_id       INT          NOT NULL,
+    country       VARCHAR(10)  NOT NULL,
+    time          VARCHAR(5)   NOT NULL,
+    emails        VARCHAR[]    NOT NULL,
+    is_container  BOOLEAN      NOT NULL,
+    email_subject VARCHAR(255) NOT NULL
 );
