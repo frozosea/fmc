5b5b7a4e71f2ccccced783a228c432b31ecfca90
dizolo <y20072004@gmail.com>
2023-05-22 17:49:31 +0800

    fix migrations

--- user-api/schema/000001_drop_tables.down.sql
+++ user-api/schema/000001_drop_tables.down.sql
@@ -1,4 +1,7 @@
 DROP TABLE IF EXISTS "bill_numbers";
 DROP TABLE IF EXISTS "containers";
+DROP TABLE IF EXISTS "company";
+DROP TABLE IF EXISTS "balance";
+DROP TABLE IF EXISTS "balance_transaction";
 DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS "feedback";
